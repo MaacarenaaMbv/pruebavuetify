f06f1255c99a628e01bfb744997b97bb9b1eebb9
MaacarenaaMbv <macarenamamolar99@gmail.com>
2023-11-27 17:16:46 +0100

    commit

---
+++ src/components/LoginComponent.vue
@@ -0,0 +1,27 @@
+<template>
+    <h1>Login Form</h1>
+    <v-sheet width="300" class="mx-auto">
+    <v-form @submit.prevent>
+      <v-text-field
+        label="Email"
+        prepend-inner-icon="mdi-email"
+      ></v-text-field>
+      <v-text-field
+        label="Password"
+        type="password"
+        prepend-inner-icon="mdi-key"
+      ></v-text-field>
+      <v-btn type="submit" block class="mt-2">Submit</v-btn>
+    </v-form>
+  </v-sheet>
+</template>
+
+<script>
+export default {
+    name: "LoginComponent"
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/MenuVuetify.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-card
+      color="grey-lighten-4"
+      flat
+      rounded="0"
+    >
+      <v-toolbar density="compact">
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+  
+        <v-toolbar-title>Title</v-toolbar-title>
+  
+        <v-spacer></v-spacer>
+
+        <v-btn to="/login" variant="outlined" color="pink-darken-4" text="Login"></v-btn>
+      </v-toolbar>
+    </v-card>
+  </template>
+
+<script>
+export default {
+  name: "MenuVuetify",
+};
+</script>
+
+<style></style>
